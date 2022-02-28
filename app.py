030b1c674c4e4a48fc5499f7f2c4bc56e966de05
sumit4singh <sumit.ipm03@gmail.com>
2022-02-28 18:23:34 -0400

    changing access key

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def storedata():
 
 
 def configures3(s3data: string):
-    session = boto3.Session(aws_access_key_id="ASIARVAB6G2YSOY4LXAS",
-                            aws_secret_access_key="scrpH8xfCUfSQDLvaUg7l3qlvlLrdIYqn6T08rKY",
+    session = boto3.Session(aws_access_key_id="ASIARVAB6G2YTG3K264O",
+                            aws_secret_access_key="okKWez82HyGEkSYwENFqyUZLwP6jFlOxwmEAaGVR",
                             aws_session_token="FwoGZXIvYXdzEK3//////////wEaDKi3SWRjH8j1aYrP7iLAAYe54sU9mLoGDEG0Tklrcp3JhmFTS0HXJlNgtxjuZKGR+oVjR0g8GdPjxaQMAGXuUp4+Slk8kWN67HezldF9Ks99S7/OZ8F1oTUE76YK+ia1wapm5FJ9rXfmVPiPRsBqVYgr1CNd2DozidLW3Ef7yGijyVTPCRfhsEqFg0C3/lOb+XC1bTcZsuoIN42aLkg3U8A4OvPgvS3tBwV9plCfJ8EkYaIH7E6esUMP38d8V7equW6RjcjQE+kSClEOSKGWwijnyvSQBjItYn/043QB0TyIbN7CNH2TD2wn6NgSV7E4wix4JB0XW4NUuQqmrNbg8Pr4eTiK"
                             )
     s3 = session.resource('s3')
