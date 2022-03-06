ecf390e305d39540f32143a243f0841f60f4e787
sumit4singh <sumit.ipm03@gmail.com>
2022-03-06 15:52:58 -0400

    changing access key

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     data = {}
     data["banner"] = "B00882103"
-    data["ip"] = "127.0.0.1"
+    data["ip"] = "172.31.16.15"
 
-    response = requests.post("http://localhost:5000/lookup", data=data)
+    # response = requests.post("http://localhost:3000/lookup", data=data)
+    response = requests.post("http://3.88.132.229:80/begin", data=data)
     return data
 
 
@@ -22,13 +23,13 @@ def storedata():
     data = request.json
     s3data: string = data.get('data')
     configures3(s3data)
-    return s3data
+    return {"s3uri": "https://cloudassignmentb00882103.s3.amazonaws.com/B00882103File.txt"}
 
 
 def configures3(s3data: string):
-    session = boto3.Session(aws_access_key_id="ASIARVAB6G2YTG3K264O",
-                            aws_secret_access_key="okKWez82HyGEkSYwENFqyUZLwP6jFlOxwmEAaGVR",
-                            aws_session_token="FwoGZXIvYXdzEK3//////////wEaDKi3SWRjH8j1aYrP7iLAAYe54sU9mLoGDEG0Tklrcp3JhmFTS0HXJlNgtxjuZKGR+oVjR0g8GdPjxaQMAGXuUp4+Slk8kWN67HezldF9Ks99S7/OZ8F1oTUE76YK+ia1wapm5FJ9rXfmVPiPRsBqVYgr1CNd2DozidLW3Ef7yGijyVTPCRfhsEqFg0C3/lOb+XC1bTcZsuoIN42aLkg3U8A4OvPgvS3tBwV9plCfJ8EkYaIH7E6esUMP38d8V7equW6RjcjQE+kSClEOSKGWwijnyvSQBjItYn/043QB0TyIbN7CNH2TD2wn6NgSV7E4wix4JB0XW4NUuQqmrNbg8Pr4eTiK"
+    session = boto3.Session(aws_access_key_id="ASIARVAB6G2YYCQN3SL6",
+                            aws_secret_access_key="mdM7o0rfFmrrlPIDH5wXl0/s17oWxIYd9mCyXb9f",
+                            aws_session_token="FwoGZXIvYXdzEDwaDGKBNLRqqgjc6WyAjCLAAagKYppHmw1IY/waDT8iKX56UYLvGx7uO1TS3ZOJivD9I3cDBNoFioDuLjSuRc98goeoEEZ3FqWHHxPBf/SIpja3UglUR1wx2Hyz7f/c1TBpYVW30QCjBDdMdy189fGx/hlMCMoIS9+wT4GFPTcP3Wi0sNscBvdWkWAYmKZG/dTyoy6alzTnnpMRbN1t57WrTLACdaIF6e8lMFfXAsAVefPjyZW+GMHYJXZuRPoNOnSQnyZjZztck2YHknCNZK7B4ijc/pORBjItKTwABzYYfqJrvBS0BmYHfI1dPqyAMLjAXY5b6gP8YDDw9X8UJWEN2yOjWOKZ"
                             )
     s3 = session.resource('s3')
     BUCKET_NAME = 'cloudassignmentb00882103'
